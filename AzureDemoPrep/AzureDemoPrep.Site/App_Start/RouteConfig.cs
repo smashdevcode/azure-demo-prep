7cde6a179381eea227a21a285e03a401693903f7
James Churchill <james@smashdev.com>
2012-10-22 22:29:32 -0700

    First pass at setting up the site

--- AzureDemoPrep/AzureDemoPrep.Site/App_Start/RouteConfig.cs
+++ AzureDemoPrep/AzureDemoPrep.Site/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace AzureDemoPrep.Site
 			routes.MapRoute(
 				name: "Default",
 				url: "{controller}/{action}/{id}",
-				defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+				defaults: new { controller = "Videos", action = "Index", id = UrlParameter.Optional }
 			);
 		}
 	}
---
+++ AzureDemoPrep/AzureDemoPrep.Site/Controllers/VideosController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AzureDemoPrep.Site.Controllers
+{
+    public class VideosController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
